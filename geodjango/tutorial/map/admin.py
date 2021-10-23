0f270a980b39d22b82930c238fdf47b755d3d733
adesukedonanika <adesukedonanika@gmail.com>
2021-10-24 01:04:36 +0900

    管理サイトに避難所を登録

--- geodjango/tutorial/map/admin.py
+++ geodjango/tutorial/map/admin.py
@@ -1,3 +1,8 @@
-from django.contrib import admin
+# from django.contrib import admin
+from django.contrib.gis import admin
+from map.models import Evacuation
 
 # Register your models here.
+
+# admin.site.registerメソッドを使ってgeomフィールドをマップ表示
+admin.site.register(Evacuation, admin.GeoModelAdmin)
