4dd8a3aebd83b3a280fc4a03fdbeb6a45885e629
adesukedonanika <adesukedonanika@gmail.com>
2021-10-23 17:23:09 +0900

    database　postgisへ切り替え

--- geodjango/tutorial/tutorial/settings.py
+++ geodjango/tutorial/tutorial/settings.py
@@ -96,13 +96,22 @@ WSGI_APPLICATION = 'tutorial.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    'defalult' : {
+        'ENGINE' : 'django.contrib.git.db.backends.postgis',
+        'NAME' : 'geodjangodb',
+        'USER' : 'django_admin',
+        'HOST' : 'localhost',
+        'PASSWORD' : 'django_admin'
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
