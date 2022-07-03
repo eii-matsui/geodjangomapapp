74b1ff72ae818c94e5d0669c55f6294ee5155123
adesu-Win10NoteGPU <adesukedonanika@gmail.com>
2022-07-03 21:17:46 +0900

    /map　urlパターンを定義

--- geodjango/tutorial/tutorial/urls.py
+++ geodjango/tutorial/tutorial/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from map import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('map/', views.Home.as_view(), name='map')#URLパターンはDjangoのpathを使う。URLパターン、対応するview関数（Classをas_viewで関数化できるメソッド）、URLパターンのの名称を設定
 ]
