4f7e7d02d695819812c6b106ab0c58e03de9cd1c
adesukedonanika <adesukedonanika@gmail.com>
2023-01-04 16:34:41 +0900

    viewでルーティング変更

--- geodjango/snapforest/snapmap/views.py
+++ geodjango/snapforest/snapmap/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+import django.views import generic
+from models import ForestSnapPhoto
+
 
 # Create your views here.
+class Home(generic.ListView):
+    model = ForestSnapPhoto
+    queryset = ForestSnapPhoto.objects.all()#テーブルに対して実行するクエリ　省略可能でdefaultで全権取得
+    template_name: str = "snapmap/index.html"
