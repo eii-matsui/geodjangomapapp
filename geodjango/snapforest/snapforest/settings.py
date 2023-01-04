46c1419e841b193417a9d2698e8f65b95169a6c3
adesukedonanika <adesukedonanika@gmail.com>
2023-01-04 16:08:31 +0900

    map をLeaflet化

--- geodjango/snapforest/snapforest/settings.py
+++ geodjango/snapforest/snapforest/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'django.contrib.gis',  #add
     'snapmap.apps.SnapmapConfig',  #add
+    'leaflet'
 ]
 
 MIDDLEWARE = [
--- geodjango/snapforest/snapmap/admin.py
+++ geodjango/snapforest/snapmap/admin.py
@@ -1,6 +1,6 @@
 #from django.contrib import admin
 from django.contrib.gis import admin
 from snapmap.models import ForestSnapPhoto
+from leaflet.admin import LeafletGeoAdmin   
 
-admin.site.register(ForestSnapPhoto, admin.GeoModelAdmin)
-
+admin.site.register(ForestSnapPhoto, LeafletGeoAdmin) 
